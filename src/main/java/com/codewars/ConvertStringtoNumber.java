4f0caa3db0ecf59683b7fe6dbfb6ca2ec83bf681
Andrzej Maliszewski <adnrzej@netmb.pl>
2023-08-31 16:35:51 +0200

    Codewars

---
+++ src/main/java/com/codewars/ConvertStringtoNumber.java
@@ -0,0 +1,8 @@
+package com.codewars;
+
+public class ConvertStringtoNumber {
+    public static void main(String[] args){
+        String str = "1234" ;
+        System.out.println(Integer.parseInt(str));
+    }
+}
