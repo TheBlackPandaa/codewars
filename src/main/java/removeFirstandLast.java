52ed5e04bbdcfdb9b94bec94c4a64e47940a2bae
Andrzej Maliszewski <adnrzej@netmb.pl>
2023-08-31 16:44:08 +0200

    Codewars

---
+++ src/main/java/removeFirstandLast.java
@@ -0,0 +1,6 @@
+public class removeFirstandLast {
+    public static void main(String[] args) {
+        String str = "eloquent";
+        System.out.println(str.substring(1,str.length()-1));
+    }
+}
